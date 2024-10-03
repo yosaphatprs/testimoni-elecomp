bfe5e88567bf5145fb0a08e655f6a619e7937e56
betgirs <elisabetgirsang@student.ub.ac.id>
2024-10-03 14:16:44 +0700

    migrations

---
+++ app/Database/Migrations/2024-10-03-071049_CreateTbDataPerusahaan.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTbDataPerusahaan extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_perusahaan' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'Nama_perusahaan' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'Alamat_perusahaan' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'Nama_peserta' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'Legalitas_before' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'Legalitas_after' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'Produk_before' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'Produk_after' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'Sertifikasi_before' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'Sertifikasi_after' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'Pengalaman_pameran_before' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'Pengalaman_pameran_after' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'Pengalaman_ekspor_before' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'Media_promosi_before' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'Media_promosi_after' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'Awal_pembinaan' => [
+                'type' => 'DATE',
+            ],
+            'Akhir_pembinaan' => [
+                'type' => 'DATE',
+            ],
+        ]);
+        $this->forge->addKey('id_perusahaan', true);
+        $this->forge->createTable('tb_data_perusahaan');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tb_data_perusahaan');
+    }
+}
---
+++ app/Database/Migrations/2024-10-03-071237_CreateTbPencapaianEkspor.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTbPencapaianEkspor extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_pencapaian_ekspor' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'id_perusahaan' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'tgl_ekspor' => [
+                'type' => 'DATE',
+            ],
+            'negara_ekspor' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'nilai_rp_ekspor' => [
+                'type' => 'DOUBLE',
+            ],
+            'nilai_usd_ekspor' => [
+                'type' => 'DOUBLE',
+            ],
+            'kuantitas_ekspor' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+            ],
+            'produk_ekspor' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'bukti_ekspor' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'nama_importir' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'jenis_ekspor' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+        ]);
+        $this->forge->addKey('id_pencapaian_ekspor', true);
+        $this->forge->addForeignKey('id_perusahaan', 'tb_data_perusahaan', 'id_perusahaan', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('tb_pencapaian_ekspor');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tb_pencapaian_ekspor');
+    }
+}
