5d2fa25f4298d916abccf8f8b78c210b9473750a
stellarmaris <bintang.pmngks@student.ub.ac.id>
2024-10-03 13:44:04 +0700

    modelperusahaan

---
+++ app/Models/PerusahaanModel.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class PerusahaanModel extends Model{
+    protected $table = 'tb_data_perusahaan';
+    protected $primaryKey = 'id_perusahaan';
+    protected $useAutoIncrement = true;
+    protected $allowedFields = [
+        'id_perusahaan',
+        'Nama_perusahaan',
+        'Alamat_perusahaan',
+        'Nama_peserta',
+        'Legalitas_before',
+        'Legalitas_after',
+        'Produk_before',
+        'Produk_after',
+        'Sertifikasi_before',
+        'Sertifikasi_after', 
+        'Pengalaman_pameran_before',
+        'Pengalaman_pameran_after',
+        'Pengalaman_ekspor_before',
+        'Media_promosi_before',
+        'Media_promosi_after',
+        'Awal_pembinaan',
+        'Akhir_pembinaan'
+    ];
+
+
+}
